72cbac5376126371f68d77c73500cba8d2d7e299
Lynne <dev@lynne.ee>
2021-10-08 05:06:55 +0200

    gitsigns: highlight lines instead of adding a signs column

--- lua/cfg_gitsigns.lua
+++ lua/cfg_gitsigns.lua
@@ -1,6 +1,10 @@
 local git_hl = require('gitsigns')
 
 git_hl.setup({
+    signcolumn = false,
+    numhl      = true,
+    linehl     = false,
+    word_diff  = false,
     yadm = {
         enable = false
     },
