c1951d29e5bf1f313b55b16b811cffdfb9b423d0
Lynne <dev@lynne.ee>
2021-11-14 13:08:40 +0100

    clangd stuff

--- lua/cfg_lsp.lua
+++ lua/cfg_lsp.lua
@@ -5,5 +5,6 @@ client_cap = require('cmp_nvim_lsp').update_capabilities(client_cap)
 
 lsp.clangd.setup({
     capabilities = client_cap,
-    cmd = { "clangd-14", "--background-index", "--pch-storage=memory" }
+    cmd = { "clangd-14", "--background-index", "--pch-storage=memory", "--inlay-hints" },
+    filetypes = { "c", "h", "cpp", "hpp", "objc", "objcpp" },
 })
